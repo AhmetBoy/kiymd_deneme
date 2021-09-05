baeb7dfbff2ec71be529ceb9ac124b41b38db7dc
AhmetBoy <50641746+AhmetBoy@users.noreply.github.com>
2021-09-05 07:37:38 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,39 @@
-from kivymd.app import MDApp
-from kivy.uix.boxlayout import BoxLayout
-from kivymd.uix.list import OneLineListItem
-class A(BoxLayout):
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.core.window import Window
+
+#Window.size = (400, 300)
+
+class FirstPage(Screen):
+    def __init__(self,**kwargs):
+        super().__init__(**kwargs)
+
+    def login_info(self):
+        
+        self.ids.lbl_info.text = ''
+        self.ids.lbl_info.text = "[color=#FF0000]username and/or password required[/color]"
+
+
+    
+
+class SecondPage(Screen):
     pass
 
-class ContentNavigationDrawer(BoxLayout):
+class ForgotPassword(Screen):
     pass
 
-class main(MDApp):
+class CreateAccount(Screen):
+    pass
+
+class WindowManager(ScreenManager):
+    pass
+
+kv = Builder.load_file("app2.kv")
+class Main(App):
     def build(self):
-        return A()
-main().run()
+        return kv
+
+if __name__ == '__main__':
+    Main().run()
